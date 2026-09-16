29baf6bc0a0de2732c8e246d910eb571a566733b
SavulescuD <savulescud13@gmail.com>
2026-09-15 20:32:06 -0400

    Renaming issue

---
+++ src/main/java/assignment1_danielsavulescu/assignment1_danielsavulescu/Key.java
@@ -0,0 +1,41 @@
+package assignment1_danielsavulescu.assignment1_danielsavulescu;
+
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+
+/**
+ *
+ * @author Daniel Savulescu
+ */
+public class Key {
+    private final Button button;
+    private final KeyCode keyCode;
+    private final String defaultStyle;
+
+    public Key(Button button, KeyCode keyCode) {
+        this.button = button;
+        this.keyCode = keyCode;
+        this.defaultStyle = button.getStyle();
+    }
+    
+    
+    public Button getButton() {
+        return button;
+    }
+
+    public KeyCode getKeyCode() {
+        return keyCode;
+    }
+    
+    /**
+     * changes animation for when the key is pressed
+     * @param button the button to be animated
+     */
+    public void buttonPressed(boolean pressed) {
+        if (pressed) {
+            button.setStyle("-fx-background-color: green");
+        } else {
+            button.setStyle(defaultStyle);
+        }
+    }
+}
---
+++ src/main/java/assignment1_danielsavulescu/assignment1_danielsavulescu/SystemInfo.java
@@ -0,0 +1,13 @@
+package assignment1_danielsavulescu.assignment1_danielsavulescu;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
