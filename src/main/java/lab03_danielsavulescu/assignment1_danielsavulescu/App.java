53b26f97739e0a013c67c5afb486e9d1c4296ef6
SavulescuD <savulescud13@gmail.com>
2026-09-15 20:30:11 -0400

    Started the App class by creating a addKey method to add the keys in a map and in a grid pane

--- src/main/java/lab03_danielsavulescu/assignment1_danielsavulescu/App.java
+++ src/main/java/lab03_danielsavulescu/assignment1_danielsavulescu/App.java
@@ -1,11 +1,13 @@
-
-
 package lab03_danielsavulescu.assignment1_danielsavulescu;
 
+import java.util.HashMap;
+import java.util.Map;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,15 +16,22 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application {
-
+    private Map<KeyCode, Key> keyMap = new HashMap<>();
+    private GridPane keyboardGrid = new GridPane();
+    
     @Override
     public void start(Stage stage) {
-        
-        
+
     }
     
     public static void main(String[] args) {
         launch();
     }
-
+    
+    public void addKey(String label, KeyCode keyCode, int col, int row) {
+        Button button = new Button(label);
+        Key key = new Key(button, keyCode);
+        keyMap.put(keyCode, key);
+        keyboardGrid.add(button, col, row);
+    }
 }
