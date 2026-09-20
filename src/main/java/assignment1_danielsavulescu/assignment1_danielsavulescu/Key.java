dee53ac92a4abfb5575ac90035c7519172eda491
SavulescuD <savulescud13@gmail.com>
2026-09-19 20:08:30 -0400

    Renamed the file

--- src/main/java/assignment1_danielsavulescu/assignment1_danielsavulescu/Key.java
+++ src/main/java/assignment1_danielsavulescu/assignment1_danielsavulescu/Key.java
@@ -8,6 +8,7 @@ import javafx.scene.input.KeyCode;
  * @author Daniel Savulescu
  */
 public class Key {
+
     private final Button button;
     private final KeyCode keyCode;
     private final String defaultStyle;
@@ -17,8 +18,7 @@ public class Key {
         this.keyCode = keyCode;
         this.defaultStyle = button.getStyle();
     }
-    
-    
+
     public Button getButton() {
         return button;
     }
@@ -26,14 +26,22 @@ public class Key {
     public KeyCode getKeyCode() {
         return keyCode;
     }
-    
+
     /**
      * changes animation for when the key is pressed
+     *
      * @param button the button to be animated
      */
     public void buttonPressed(boolean pressed) {
         if (pressed) {
-            button.setStyle("-fx-background-color: green");
+            button.setStyle(
+                    "-fx-background-color: #52A6DE;"
+                    + "-fx-border-color: #DE5260;"
+                    + "-fx-border-radius: 8;"
+                    + "-fx-background-radius: 8;"
+                    + "-fx-border-width: 2"
+            );
+
         } else {
             button.setStyle(defaultStyle);
         }
